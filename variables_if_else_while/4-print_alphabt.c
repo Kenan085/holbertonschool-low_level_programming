8bb6e24ff4dee66ee5b9f67ef88570bc13e372e7
Kenan <kenanshafizade85@gmail.com>
2024-02-23 13:04:53 +0000

    Uptaded

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -True
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(letter);
+			putchar("\n");
+		}
+	}
+	return (0);
+}

a1574b8a0f2d40c8e50c866cd15cdaac37dd804a
Kenan <kenanshafizade85@gmail.com>
2024-02-26 11:53:53 +0000

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,16 +11,16 @@ void more_numbers(void)
 
 	while (num > 0)
 	{
-		int a = 14;
+		int a = 0;
 
-		while (a >= 0)
+		while (a <= 14)
 		{
 			if (a >= 10 && a <= 14)
 			{
 				_putchar('1');
 			}
 			_putchar(a % 10 + '0');
-			a--;
+			a++;
 		}
 		_putchar('\n');
 		num--;

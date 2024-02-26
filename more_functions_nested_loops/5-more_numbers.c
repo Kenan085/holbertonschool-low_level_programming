49e24d2f9c23570d80a0d1833a018c1f623517e5
Kenan <kenanshafizade85@gmail.com>
2024-02-26 11:51:56 +0000

    Task 5 and 6

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * from 0 to 14
+ * Return: table of numbers
+ */
+void more_numbers(void)
+{
+	int num = 10;
+
+	while (num > 0)
+	{
+		int a = 14;
+
+		while (a >= 0)
+		{
+			if (a >= 10 && a <= 14)
+			{
+				_putchar('1');
+			}
+			_putchar(a % 10 + '0');
+			a--;
+		}
+		_putchar('\n');
+		num--;
+	}
+}

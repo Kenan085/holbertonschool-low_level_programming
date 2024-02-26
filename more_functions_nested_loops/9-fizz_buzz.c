903469d5884b5a1f35f212c23802ae03d84de3de
Kenan <kenanshafizade85@gmail.com>
2024-02-26 17:59:04 +0400

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz babyyyy
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf ("FizzBuzz");
+		} else if (num % 5 == 0 && num % 3 != 0)
+		{
+			printf ("Buzz");
+		} else if (num % 3 == 0 && num % 5 != 0)
+		{
+			printf ("Fizz");
+		}
+		else
+		{
+			printf (num);
+		}
+		printf(" ");
+		num++;
+	}
+}

159160238ece8bb1e9de3c2a9ef51889eb977406
Kenan <kenanshafizade85@gmail.com>
2024-02-27 12:41:26 +0000

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,21 @@
  */
 void print_triangle(int size)
 {
+	int i, j, k, z;
 
+	for (i = 1; i <= size; i++)
+	{
+		k = size - i;
+		for (j = k; j >= 1; j--)
+		{
+			_putchar(' ');
+		}
+
+		for (z = 1; z <= i; z++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
 }

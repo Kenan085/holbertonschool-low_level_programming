ceabbdf09d47259f007884cd82f28e6453f4602e
Kenan <kenanshafizade85@gmail.com>
2024-03-03 09:27:03 +0000

    jej

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,27 +7,19 @@
  */
 void rev_string(char *s)
 {
-	char *ends, *begins;
-	int n, i = 0;
+	char *charr;
+	int i = 0;
 
-	n = 0;
-	while (s[n] != '\0' && s[n + 1] != '\0')
+	while (*s != '\0')
 	{
-		ends++;
-		n++;
-	}
-	n = n + 1;
-	begins = s;
-
-	while (i < n / 2)
-	{
-		char str;
-
-		str = *ends;
-		*ends = *begins;
-		*begins = str;
-		begins++;
-		ends--;
+		*charr = *s;
+		s++;
+		charr++;
 		i++;
+	}
+	for (; i > 0; i--)
+	{
+		*s = *(charr - i);
+		s--;
 	}
 }

ff0f224b09b0bb82f066fcbccf209ef9915fe85d
Kenan <kenanshafizade85@gmail.com>
2024-03-03 07:14:37 +0000

    My first commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen -  a function that returns the length of a string
+ * @s: character
+ * Return: length of a srring character
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}

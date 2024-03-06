2bcf0ad4b71c5f1fa474d480bc3ec7c369fe8320
Kenan <kenanshafizade85@gmail.com>
2024-03-06 20:45:23 +0000

    kt

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -4,7 +4,7 @@
  * print_diagsums - a function that prints the sum of the two diagonals
  * of a square matrix of integers.
  * @a: our array
- * @size of array
+ * @size: of array
  * Return: sum of the left and write diagonals
  */
 void print_diagsums(int *a, int size)
@@ -15,6 +15,10 @@ void print_diagsums(int *a, int size)
 
 	for (n = 0; n < size; n++)
 	{
+		left_d += a[(size + 1) * n];
+		right_d += a[(size - 1) * (n + 1)];
+	}
+	printf("%d, %d\n", left_d, right_d);
 
 
 }

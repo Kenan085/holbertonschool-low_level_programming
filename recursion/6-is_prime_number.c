9ed8d30f8f6077c41e1dbe9bb632038775b5aa18
Kenan <kenanshafizade85@gmail.com>
2024-03-09 23:01:13 +0000

    hehe

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _isprime - Function that checks the number as prime number
+ * @num: The num
+ * @operand: The number that represents the operand number
+ */
+int _isprime(unsigned int num, unsigned int operand)
+{
+	if (num % operand == 0)
+	{
+		if (num == operand)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0 + _isprime(num, operand + 1));
+}
+
+/**
+ * is_prime_number - Function that returns 1 if the input integer is
+ * a prime number, otherwise return 0
+ * @n: The parameter to be checked as prime number
+ * Return: Returns 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		return (0);
+	}
+	if (n <= 0)
+	{
+		return (0);
+	}
+	return (_isprime(n, 2));
+}

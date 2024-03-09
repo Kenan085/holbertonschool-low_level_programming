1407115fd310c2ca3e8bb7d684fb5ede206c7a58
Kenan <kenanshafizade85@gmail.com>
2024-03-09 21:55:05 +0000

    maybe last

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,19 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
 	{
-		if (*s == c)
+		if (*(s + i) == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
 	}
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+
 	return (NULL);
 }

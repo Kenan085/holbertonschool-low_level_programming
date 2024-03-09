34dfadb35aaba3e8b7f959a1c15c204cb84bd9f8
Kenan <kenanshafizade85@gmail.com>
2024-03-09 23:59:15 +0000

    reest

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *main - Function that adds positive numbers
+ *@argc: The parameter that represents the count of arguments
+ *@argv: The parameter that represents the array of the characters
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

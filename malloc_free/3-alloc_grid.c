2bc820399874ba9983a81592373a103c96bc6c89
Kenan <kenanshafizade85@gmail.com>
2024-03-17 08:56:30 +0000

    Task 3 done?

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	arr = (int **) malloc(height * sizeof(int *));
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 	{
 		free(arr);

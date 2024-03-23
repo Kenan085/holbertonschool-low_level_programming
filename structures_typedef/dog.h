14e1495dcd0051a227b7fb5511c2d7630bdecdc8
Kenan <kenanshafizade85@gmail.com>
2024-03-23 17:51:07 +0000

    2nd

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - creates dog struct
@@ -19,5 +20,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
